2b5fcdbca2ae6749c1c31dfa475b75765773f642
rajat106p <rajat106p@gmail.com>
2023-09-01 19:44:32 +0530

    fixed test cases issues.

--- TestingAppProject/__tests__/Dashboard.test.js
+++ TestingAppProject/__tests__/Dashboard.test.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import {render, waitFor} from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import axios from 'axios';
-import {Dashboard} from '../src/screen/dashboard';
+import { Provider } from 'react-redux';
+
+import { Dashboard } from '../src/screen/dashboard';
+import store from '../src/store';
+import { LOADER_STATUS, POST_LIST, SET_POST_LIST } from "../src/store/constant";
+
 
 jest.mock('axios');
 const mockResponse = [
-  {id: 1, title: 'Mock Title 1', body: 'Mock Body 1'},
-  {id: 2, title: 'Mock Title 2', body: 'Mock Body 2'},
+  { id: 1, title: 'Mock Title 1', body: 'Mock Body 1' },
+  { id: 2, title: 'Mock Title 2', body: 'Mock Body 2' },
 ];
 describe('Dashboard component', () => {
   test('renders correctly', () => {
-    const {getByText} = render(<Dashboard />);
+    const { getByText } = render(<Provider store={store}><Dashboard /></Provider>);
     const headerTxt = getByText('Welcome to Dashboard');
     const refreshButton = getByText('Refresh');
     expect(headerTxt).toBeTruthy();
     expect(refreshButton).toBeTruthy();
   });
   test('fetches and displays data correctly', async () => {
-    axios.get.mockResolvedValue({data: mockResponse});
-    const {findByText} = render(<Dashboard />);
+    axios.get.mockResolvedValue({ data: mockResponse });
+    store.dispatch({ type: POST_LIST, response: mockResponse })
+    const state = store.getState()
+    const { findByText } = render(<Provider store={store}><Dashboard /></Provider>);
     const title1 = await findByText('Mock Title 1');
     const body1 = await findByText('Mock Body 1');
     expect(title1).toBeTruthy();
@@ -29,9 +36,9 @@ describe('Dashboard component', () => {
     expect(body2).toBeTruthy();
   });
   test('navigates to Listdata on item press', async () => {
-    axios.get.mockResolvedValue({data: mockResponse});
-    const navigationMock = {navigate: jest.fn()};
-    const {findByText} = render(<Dashboard navigation={navigationMock} />);
+    axios.get.mockResolvedValue({ data: mockResponse });
+    const navigationMock = { navigate: jest.fn() };
+    const { findByText } = render(<Provider store={store}><Dashboard navigation={navigationMock} /></Provider>);
     await waitFor(() => expect(findByText('Mock Title 1')).toBeTruthy());
     expect(navigationMock.navigate).toBeTruthy();
   });
